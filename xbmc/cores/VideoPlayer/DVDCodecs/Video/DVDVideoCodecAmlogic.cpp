0a90e0ac2425bfd6ac5511c79ea4603917ecf296
Portisch <hugo.portisch@yahoo.de>
2023-04-06 21:07:55 +0200

    libdovi: use and adjust libdovi for Amlogic linux platform

--- xbmc/cores/VideoPlayer/DVDCodecs/Video/DVDVideoCodecAmlogic.cpp
+++ xbmc/cores/VideoPlayer/DVDCodecs/Video/DVDVideoCodecAmlogic.cpp
@@ -314,6 +314,8 @@ bool CDVDVideoCodecAmlogic::Open(CDVDStreamInfo &hints, CDVDCodecOptions &option
         {
           bool user_dv_disable = CServiceBroker::GetSettingsComponent()->GetSettings()->GetBool(
               CSettings::SETTING_COREELEC_AMLOGIC_DV_DISABLE);
+          enum DOVIMode convertDovi = static_cast<enum DOVIMode>(CServiceBroker::GetSettingsComponent()->GetSettings()->GetInt(
+              CSettings::SETTING_VIDEOPLAYER_CONVERTDOVI));
 
           if (!user_dv_disable && CServiceBroker::GetSettingsComponent()->GetSettings()->GetInt(
                   CSettings::SETTING_COREELEC_AMLOGIC_DV_LED) == AML_DV_TV_LED)
@@ -321,6 +323,22 @@ bool CDVDVideoCodecAmlogic::Open(CDVDStreamInfo &hints, CDVDCodecOptions &option
             m_bitstream->SetDoviZeroLevel5(
                 CServiceBroker::GetSettingsComponent()->GetSettings()->GetBool(
                     CSettings::SETTING_COREELEC_AMLOGIC_DOVIZEROLEVEL5));
+          }
+
+          if ((m_hints.dovi.dv_profile == 4 || m_hints.dovi.dv_profile == 7) && (convertDovi > DOVIMode::MODE_LOSSLESS) && !user_dv_disable)
+          {
+            CLog::Log(LOGINFO, "{}::{} - HEVC bitstream profile {} will be converted to profile {}", __MODULE_NAME__, __FUNCTION__,
+              m_hints.dovi.dv_profile,
+              convertDovi == DOVIMode::MODE_TOMEL ? "7 minimum enhancement layer" :
+             (convertDovi == DOVIMode::MODE_TO81 || convertDovi == DOVIMode::MODE_PROFILE5TO81) ?  "8.1" :
+              convertDovi == DOVIMode::MODE_TO84 ?  "8.4" :
+              convertDovi == DOVIMode::MODE_TO81MAPPINGPRESERVED ?  "8.1 mapping reserved" : "unknown");
+
+            if (convertDovi > DOVIMode::MODE_TOMEL)
+              m_hints.dovi.dv_profile = 8;
+
+            m_hints.dovi.el_present_flag = false;
+            m_bitstream->SetConvertDovi(convertDovi);
           }
         }
       }
