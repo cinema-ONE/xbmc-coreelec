glsimon666/CE-NO-XBMC

VideoPlayer: get hardware buffer data size When buffer queue runs empty check hardware decoder buffer.

xbmc/cores/VideoPlayer/VideoPlayerVideo.cpp
@@ -305,7 +305,15 @@ bool CVideoPlayerVideo::AcceptsData() const
 
 bool CVideoPlayerVideo::HasData() const
 {
-  return m_messageQueue.GetDataSize() > 0;
+  bool ret;
+
+  if (!(ret = (m_messageQueue.GetDataSize() > 0)))
+  {
+    if (m_pVideoCodec && m_processInfo.IsVideoHwDecoder())
+      ret = m_pVideoCodec->GetDataSize() > 0;
+  }
+
+  return ret;
 }
 
 bool CVideoPlayerVideo::IsInited() const
@@ -1085,8 +1093,10 @@ std::string CVideoPlayerVideo::GetPlayerInfo()
   std::ostringstream s;
   int width, height;
   m_processInfo.GetVideoDimensions(width, height);
-  s << "vq:" << std::setw(2) << std::min(99, m_messageQueue.GetLevel()) << "% (" << std::setw(2) << std::min(99, m_messageQueue.GetLevel(true)) << "%)";
-  s << std::fixed << std::setprecision(3) << m_messageQueue.GetTimeSize();
+  s << "vq:" << std::setw(2) << std::min(99, m_messageQueue.GetLevel()) << "% (" << std::setw(2) << std::min(99, m_messageQueue.GetLevel(true));
+  if (m_pVideoCodec && m_processInfo.IsVideoHwDecoder())
+    s << "%/" << std::setw(2) << std::min(99, m_pVideoCodec->GetDataLevel());
+  s << "%)" << std::fixed << std::setprecision(3) << m_messageQueue.GetTimeSize();
   s << "s, Mb/s:" << std::fixed << std::setprecision(2)
     << static_cast<double>(GetVideoBitrate()) / (1024.0 * 1024.0);
   s << ", dc:"   << m_processInfo.GetVideoDecoderName().c_str();

cinema-ONE/xbmc-coreelec

xbmc/cores/VideoPlayer/VideoPlayerVideo.cpp
@@ -305,7 +305,15 @@ bool CVideoPlayerVideo::AcceptsData() const
 
 bool CVideoPlayerVideo::HasData() const
 {
-  return m_messageQueue.GetDataSize() > 0;
+  bool ret;
+
+  if (!(ret = (m_messageQueue.GetDataSize() > 0)))
+  {
+    if (m_pVideoCodec && m_processInfo.IsVideoHwDecoder())
+      ret = m_pVideoCodec->GetDataLevel() > 0;
+  }
+
+  return ret;
 }
 
 bool CVideoPlayerVideo::IsInited() const
@@ -1088,8 +1096,10 @@ std::string CVideoPlayerVideo::GetPlayerInfo()
   std::ostringstream s;
   int width, height;
   m_processInfo.GetVideoDimensions(width, height);
-  s << "vq:" << std::setw(2) << std::min(99, m_messageQueue.GetLevel()) << "% (" << std::setw(2) << std::min(99, m_messageQueue.GetLevel(true)) << "%)";
-  s << std::fixed << std::setprecision(3) << m_messageQueue.GetTimeSize();
+  s << "vq:" << std::setw(2) << std::min(99, m_messageQueue.GetLevel()) << "% (" << std::setw(2) << std::min(99, m_messageQueue.GetLevel(true));
+  if (m_pVideoCodec && m_processInfo.IsVideoHwDecoder())
+    s << "%/" << std::setw(2) << std::min(99, m_pVideoCodec->GetDataLevel());
+  s << "%)" << std::fixed << std::setprecision(3) << m_messageQueue.GetTimeSize();
   s << "s, Mb/s:" << std::fixed << std::setprecision(2)
     << static_cast<double>(GetVideoBitrate()) / (1024.0 * 1024.0);
   s << ", dc:"   << m_processInfo.GetVideoDecoderName().c_str();